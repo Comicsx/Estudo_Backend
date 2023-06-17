3d48ae4d2b0b7b3340758719413585198ff756d8
Henrique <henriquecardosopacheco88@gmail.com>
2023-06-17 11:27:00 -0300

    CRIAÇÃO DE UM CONTROLLER E DIVISÃO DE RESPONSABILIDADES

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,12 @@
 const { Router } = require("express");
-
 const userRoutes = Router();
 
+const UserController = require("../controllers/UserController"); /* Por ser uma classe eu tenho que deixar ela em memória */
+const userController = new UserController /* Deixei a minha classe em memória */
 
-userRoutes.post("/", (request, response) => {
-    const { name, email, password } = request.body;
 
-    response.json({name, email, password})
 
-});
+userRoutes.post("/", userController.create)
+
 
 module.exports = userRoutes;
